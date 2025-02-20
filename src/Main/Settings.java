3e0f776aee42a9b46e9e54b231546115abc68909
Mad0 <jose.antonio.pepe@icloud.com>
2025-02-20 17:35:23 -0600

    create configuration layout

--- src/Main/Settings.java
+++ src/Main/Settings.java
@@ -4,4 +4,11 @@ import javax.swing.*;
 
 public class Settings extends JFrame {
     public JPanel panel;
+    private JTable users;
+    private JButton newUser;
+    private JTextField password;
+    private JTextField ssid;
+    private JButton update;
+    private JTable machines;
+    private JButton newMachine;
 }
